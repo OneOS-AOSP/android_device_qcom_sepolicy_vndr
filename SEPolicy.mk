b371a531897b6d54dd9f5ac6781c3369582b6bed
BadWolf-Alfa <marco@lunawolf.es>
2021-09-05 11:00:37 +0200

    ONEfy

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -76,4 +76,4 @@ ifneq (,$(filter sdm845 sdm710 sdm660 msm8937 msm8953, $(TARGET_BOARD_PLATFORM))
     endif
 endif
 
--include device/nad/sepolicy/qcom/sepolicy.mk
+-include device/one/sepolicy/qcom/sepolicy.mk
